dd431402c3a1e4a8bd80b2206b02a2fe49719d86
TEAMvirus <virusteam@doctor.com>
2016-07-27 05:21:15 -0700

    Update Stats.lua

--- plugins/Stats.lua
+++ plugins/Stats.lua
@@ -143,8 +143,8 @@ return {
     "^[#!/]([Ss]tats)$",
     "^[#!/]([Ss]tatslist)$",
     "^[#!/]([Ss]tats) (group) (%d+)",
-    "^[#!/]([Ss]tats) (signal)",
-	"^[#!/]([Ss]ignal)"
+    "^[#!/]([Ss]tats) (aghrab)",
+	"^[#!/]([Aa]ghrab)"
     }, 
   run = run
 }
